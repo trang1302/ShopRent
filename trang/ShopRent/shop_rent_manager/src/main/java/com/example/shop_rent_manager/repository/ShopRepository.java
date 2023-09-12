7e00ecb9b5d8b2e42971d8f199b8bc19bd1baa0a
trang1302 <131360715+trang1302@users.noreply.github.com>
2023-09-12 08:44:23 +0700

    Delete trang/ShopRent/shop_rent_manager directory

--- trang/ShopRent/shop_rent_manager/src/main/java/com/example/shop_rent_manager/repository/ShopRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.shop_rent_manager.repository;
-
-import com.example.shop_rent_manager.model.Shop;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ShopRepository extends JpaRepository<Shop, Long> {
-}
--- trang/ShopRent/shop_rent_manager/src/main/java/com/example/shop_rent_manager/repository/UserRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.shop_rent_manager.repository;
-import com.example.shop_rent_manager.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
